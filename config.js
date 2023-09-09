f1c133c1686f477ccd2d2140382b17df1cb5a7eb
ZamingUwU <99128630+ZamingUwU@users.noreply.github.com>
2023-09-09 22:04:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '', // Write here your API Key
+	weatherKey: 'afc6cafd6d00ae3048bfee79995dcd6e', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'fr', // More languages in https://openweathermap.org/current#multi
